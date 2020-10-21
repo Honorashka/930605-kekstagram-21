572fee6831965a2be57783249044498fe9fef7c9
honorashka <honor353@yandex.ru>
2020-10-22 00:19:02 +0300

    отправка формы

--- js/validation.js
+++ js/validation.js
@@ -4,6 +4,7 @@
   // Валидация хэштегов
 
   const inputHashTags = document.querySelector('.text__hashtags');
+  const textAreaComment = document.querySelector('.text__description');
 
   const onInputHashTags = () => {
     const hashtags = inputHashTags.value.split('');
@@ -12,11 +13,11 @@
       re.test(hashtags[i]);
       if (!re.test(hashtags[i])) {
         inputHashTags.setCustomValidity('Хэштег должен начинаться с #, и содержать только буквы и цифры.');
-      } else if (hashtags[i].length < window.hashtag.MIN_LENGTH_HASHTAG) {
+      } else if (hashtags[i].length <= window.hashtag.MIN_LENGTH_HASHTAG) {
         inputHashTags.setCustomValidity('Добавьте ' + (window.hashtag.MIN_LENGTH_HASHTAG - hashtags[i].length) + ' симв.');
       } else if (hashtags[i].length > window.hashtag.MAX_LENGHT_HASHTAG) {
         inputHashTags.setCustomValidity('Удалите ' + (hashtags[i].length - window.hashtag.MAX_LENGHT_HASHTAG) + ' симв.');
-      } else if (hashtags.length > 5) {
+      } else if (hashtags.length >= 5) {
         inputHashTags.setCustomValidity('Можно указывать не более 5 хэштегов');
       } else {
         inputHashTags.setCustomValidity('');
@@ -29,6 +30,7 @@
 
   window.validation = {
     inputHashTags: inputHashTags,
+    textAreaComment: textAreaComment,
   };
 
 })();
